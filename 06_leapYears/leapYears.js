b64c9c0d969944733b4a6514d2f682277154ec94
Harman <haskhatkar@gmail.com>
2024-06-27 19:17:09 -0700

    Solved.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,7 @@
-const leapYears = function() {
+const leapYears = (date) => {
+    return (date % 4 === 0 && ((date % 100 !== 0) || (date % 400 === 0)));
+}
 
-};
 
 // Do not edit below this line
 module.exports = leapYears;
