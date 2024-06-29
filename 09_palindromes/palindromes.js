b7718eecb651ad86eb9f0b699d775b06b2aeb4e3
Harman <haskhatkar@gmail.com>
2024-06-28 18:21:25 -0700

    solved

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,18 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const alphanumericals = 'abcdefghijklmnopqrstuvwxyz0123456789';
+
+    const cleanedString = string
+        .toLowerCase()
+        .split('')
+        .filter((character) => alphanumericals.includes(character))
+        .join('');
+
+    const reversedString = cleanedString.split('').reverse().join('');
+
+    return reversedString === cleanedString;
 
 };
+    
 
 // Do not edit below this line
 module.exports = palindromes;
